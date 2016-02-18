3bcb1ad6fdfea976ce7a00f3318f0e029691c127
Olliecad1 <officialgamermorris@gmail.com>
2016-02-18 22:10:17 +0000

    I have added some code

---
+++ Py Scripts/part1.py
@@ -0,0 +1,39 @@
+# this is a comment when you run this program this comment wont appear on the screen you do need these comment but most programs have them 
+# the reason we have comments is because they are used by the program creator to document what is happening
+# so that when someone else picks up the program and they cant understand the code then they will understand because of the comments
+# The next line of code is the first that will appear on the screen the print statement
+print('Hello everyone')
+# the next section of code with be numbers and maths
+# Be aware none of maths stuff will work unless you print them out or your using the idle command window for example for the print one see line below
+print(1 + 2)
+# * is multiply in python 
+print(4 * 2)
+# / is division in python
+print(4 / 2)
+# you dont have to do whole numbers you can do fractions
+print(4.4 * 2.2)
+print(4.2 ** 2.2)
+# Variables are nothing but reserved memory locations to store values. This means that when you create a variable you reserve some space in memory.
+#Based on the data type of a variable, the interpreter allocates memory and
+#decides what can be stored in the reserved memory. Therefore,
+#by assigning different data types to variables, you can store integers,
+#decimals or characters in these variables.
+number = 20
+variable = 5 * 5
+print(number * variable)
+print(3 * 3 / 3)
+# some more basic variables
+a = 1
+print(a)
+hello = 'hello'
+print(hello)
+
+wrong = 1
+b = 1
+print('3 is',wrong + b)
+
+print(number * variable)
+
+variable1 = 'Hello'
+world = ' world'
+print(variable1 + world)
